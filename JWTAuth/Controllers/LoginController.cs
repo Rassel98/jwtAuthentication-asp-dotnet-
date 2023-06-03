916e2ef14d9d761f808c6fc3e87b9d4b74efcdd9
rassel <indonesiarassel@gmail.com>
2023-06-03 17:40:39 +0600

    first

---
+++ JWTAuth/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using JWTAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+        private User Authentication(User user)
+        {
+            User _user = null;
+            if(user.Username=="admin" && user.Password=="admin")
+            {
+                _user = new User { Username="Md Jahidul Ahsan Rassel"};
+            }
+            return _user;
+        }
+        private String GenerateToken(User users)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var cretential=new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
+
+            var token=new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],null,expires:DateTime.Now.AddMinutes(1),signingCredentials:cretential);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(User user)
+        {
+            IActionResult response = Unauthorized();
+            var logUser=Authentication(user);
+            if (logUser!=null)
+            {
+                var tokens=GenerateToken(user);
+                response = Ok(new {message ="Successfully Login ",token=tokens});
+            }
+            return response;
+
+
+        }
+    }
+}
---
+++ JWTAuth/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using JWTAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public String GetData()
+        {
+            return "Authenticated with jwt";
+        }
+        [HttpGet]
+        [Route("GetData2")]
+        public String GetData2()
+        {
+            return "withoud un Authenticated with jwt";
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("AddUser")]
+        public String AddUser(User user)
+        {
+            return "Authenticated with jwt " + user.Username;
+        }
+    }
+}
